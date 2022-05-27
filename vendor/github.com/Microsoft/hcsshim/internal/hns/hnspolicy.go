911a101a89b5ea1153b5cfd69d04b593d7743e63
Ameya Gawde <agawde@mirantis.com>
2022-05-26 20:47:46 -0700

    Bump hcsshim
    Signed-off-by: Ameya Gawde <agawde@mirantis.com>

--- vendor/github.com/Microsoft/hcsshim/internal/hns/hnspolicy.go
+++ vendor/github.com/Microsoft/hcsshim/internal/hns/hnspolicy.go
@@ -17,13 +17,15 @@ const (
 	OutboundNat          PolicyType = "OutBoundNAT"
 	ExternalLoadBalancer PolicyType = "ELB"
 	Route                PolicyType = "ROUTE"
+	Proxy                PolicyType = "PROXY"
 )
 
 type NatPolicy struct {
-	Type         PolicyType `json:"Type"`
-	Protocol     string
-	InternalPort uint16
-	ExternalPort uint16
+	Type                 PolicyType `json:"Type"`
+	Protocol             string
+	InternalPort         uint16
+	ExternalPort         uint16
+	ExternalPortReserved bool       `json:",omitempty"`
 }
 
 type QosPolicy struct {
@@ -55,8 +57,18 @@ type PaPolicy struct {
 
 type OutboundNatPolicy struct {
 	Policy
-	VIP        string   `json:"VIP,omitempty"`
-	Exceptions []string `json:"ExceptionList,omitempty"`
+	VIP          string   `json:"VIP,omitempty"`
+	Exceptions   []string `json:"ExceptionList,omitempty"`
+	Destinations []string `json:",omitempty"`
+}
+
+type ProxyPolicy struct {
+	Type          PolicyType `json:"Type"`
+	IP            string     `json:",omitempty"`
+	Port          string     `json:",omitempty"`
+	ExceptionList []string   `json:",omitempty"`
+	Destination   string     `json:",omitempty"`
+	OutboundNat   bool       `json:",omitempty"`
 }
 
 type ActionType string
